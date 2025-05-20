4a8726fb6e889bfa47480d5b07e30d56eea2d03c
mrvangrant <halexmota@gmail.com>
2025-05-20 14:31:48 +0100

    Sprites

--- JogoTecnicas/Game1.cs
+++ JogoTecnicas/Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using JogoTecnicas.Graficos;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -11,7 +12,9 @@ namespace JogoTecnicas
 
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        
+
+        private Texture2D _spriteSheetTexture;
+
 
         public Game1()
         {
@@ -32,6 +35,8 @@ namespace JogoTecnicas
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            _spriteSheetTexture = Content.Load<Texture2D>(ASSET_NAME_SPRITESHEET);
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -49,6 +54,17 @@ namespace JogoTecnicas
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+
+            Sprite Correr1 = new Sprite(_spriteSheetTexture, 6, 0, 42, 61);
+
+            Correr1.Draw(_spriteBatch, new Vector2(10, 10));
+
+           // _spriteBatch.Draw(_spriteSheetTexture, new Vector2(10, 10),new Rectangle(6,0,42,61), Color.White);
+
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
